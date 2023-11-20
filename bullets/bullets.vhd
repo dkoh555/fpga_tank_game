5456f32cb55b1bd24fe345decf714498e17fd7d9
onecholas <nickhsu2025@u.northwestern.edu>
2023-11-20 14:12:43 -0600

    Tested and fixed bullets

--- bullets/bullets.vhd
+++ bullets/bullets.vhd
@@ -2,98 +2,130 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use WORK.tank_const.all;
 
 -- Bullet entities
 entity bullet is
 
     port (
+        -- Inputs
         bullet_move : in std_logic;
+        rst : in std_logic;
+        rst_hit : in std_logic;
         shoot : in std_logic;
         curr_tank_x : in std_logic_vector (9 downto 0);
         curr_tank_y : in std_logic_vector (9 downto 0);
         other_tank_x : in std_logic_vector (9 downto 0);
         other_tank_y : in std_logic_vector (9 downto 0);
-        rst : in std_logic;
+        -- Outputs
         bullet_x : out std_logic_vector (9 downto 0);
         bullet_y: out std_logic_vector (9 downto 0);
-        hit : out std_logic;
-    );
-    generic (
-        screen_width : natural := 640;
-        screen_height : natural := 480;
-        tank_width: natural := 20;
-        tank_height: natural := 10;
+        hit : out std_logic
     );
 
 end entity bullet;
 
 architecture behavioral of bullet is
-    internal_shoot_status, internal_bullet_status : std_logic;
+    
+    signal internal_shoot_status, internal_bullet_valid, internal_hit : std_logic;
+    signal curr_bullet_x, curr_bullet_y : std_logic_vector (9 downto 0);
 
-    function get_collision (signal other_tank_x, other_tank_y, tank_width, tank_height, bullet_x, bullet_y: std_logic_vector) return std_logic is
+    function get_collision (signal other_tank_x, other_tank_y, bullet_x, bullet_y: std_logic_vector) return std_logic is
 		variable collision : std_logic;
-		
 	begin
         -- If within tank boundaries
-        if (unsigned(other_tank_x) - to_unsigned(tank_width/2) <= bullet_x and 
-            unsigned(other_tank_x) + to_unsigned(tank_width/2) >= bullet_x and
-            unsigned(other_tank_y) - to_unsigned(tank_height/2) <= bullet_y and
-            unsigned(other_tank_y) + to_unsigned(tank_height/2) >= bullet_y) then
-                collision <= '1';
+        if ( (unsigned(other_tank_x) - to_unsigned(TANK_WIDTH/2, other_tank_x'length) <= unsigned(bullet_x)) and 
+             (unsigned(other_tank_x) + to_unsigned(TANK_WIDTH/2, other_tank_x'length) >= unsigned(bullet_x)) and
+             (unsigned(other_tank_y) - to_unsigned(TANK_HEIGHT/2, other_tank_y'length) <= unsigned(bullet_y)) and
+             (unsigned(other_tank_y) + to_unsigned(TANK_HEIGHT/2, other_tank_y'length) >= unsigned(bullet_y))) then
+                collision := '1';
         else 
-                collision <= '0';
+                collision := '0';
         end if;
         return collision;
 	end function get_collision;
 
 begin
 
-    process (shoot, bullet_move, rst) begin
-        if (rst = '1') then
-            internal_bullet_status <= '0';
+    process (internal_bullet_valid, shoot) begin
+        -- If reset
+        if (internal_bullet_valid = '0') then
+            -- Set status to not shot
             internal_shoot_status <= '0';
-            hit <= '0';
-            bullet_x <= curr_tank_x;
-            bullet_y <= curr_tank_y;
-        -- If bullet is shot
+        -- If bullet valid
         elsif (rising_edge(shoot)) then
-            -- If bullet is currently not valid, shoot
-            if (internal_shoot_status = '0' and internal_bullet_status) then
-                internal_shoot_status <= '1';
-            end if;
-        -- If bullet is called to move
+            -- Set status to shot
+            internal_shoot_status <= '1';
+        end if;
+    end process;
+    
+    process (rst, rst_hit, internal_shoot_status, bullet_move) begin
+        if (rst_hit = '1' or rst = '1') then
+            internal_hit <= '0';
+            -- Set x value of bullet to tank
+            curr_bullet_x <= curr_tank_x;
+            -- Set y value of bullet to tank
+            curr_bullet_y <= curr_tank_y;
+            -- Bullet is now valid
+            internal_bullet_valid <= '0';
+        -- If bullet is called to move and bullet has been shot
+        elsif (internal_shoot_status = '0' and internal_hit = '0') then
+            internal_hit <= '0';
+            -- Set x value of bullet to tank
+            curr_bullet_x <= curr_tank_x;
+            -- Set y value of bullet to tank
+            curr_bullet_y <= curr_tank_y;
+            -- Bullet is now valid
+            internal_bullet_valid <= '1';
         elsif (rising_edge(bullet_move)) then
-            -- If bullet has been shot
+            -- If status is shot
             if (internal_shoot_status = '1') then
                 -- If bullet has collided with other tank
-                if (get_collision(other_tank_x, other_tank_y, tank_width, tank_height, bullet_x, bullet_y) = '1') then 
-                    -- Set internal_bullet_status to 0 since it is no longer valid
-                    internal_bullet_status <= '0';
+                if (get_collision(other_tank_x, other_tank_y, curr_bullet_x, curr_bullet_y) = '1') then 
+                    -- Set x value of bullet to tank
+                    curr_bullet_x <= curr_tank_x;
+                    -- Set y value of bullet to tank
+                    curr_bullet_y <= curr_tank_y;
                     -- Set hit to 1
-                    hit <= '1';
+                    internal_hit <= '1';
+                    -- Bullet is no longer valid
+                    internal_bullet_valid <= '0';
                 -- If bullet is still in a valid location
-                elsif (bullet_x <= screen_width and bullet_x >= 0 and bullet_y <= screen_height and bullet_y >= 0)
-                    if (curr_tank_y < screen_height/2) then
+                elsif (unsigned(curr_bullet_x) <= to_unsigned(SCREEN_WIDTH, curr_bullet_x'length) and 
+                        unsigned(curr_bullet_x) >= to_unsigned(0, curr_bullet_x'length) and
+                        unsigned(curr_bullet_y) <= to_unsigned(SCREEN_HEIGHT, curr_bullet_y'length) and 
+                        unsigned(curr_bullet_y) >= to_unsigned(0, curr_bullet_x'length)) then
+                    if (unsigned(curr_tank_y) < to_unsigned(SCREEN_HEIGHT/2, curr_tank_y'length)) then
                         -- Move bullet by 1
-                        bullet_y <= std_logic_vector(unsigned(bullet_y) + 1);
+                        curr_bullet_y <= std_logic_vector(unsigned(curr_bullet_y) + 1);
                     else
                         -- Move bullet by -1
-                        bullet_y <= std_logic_vector(unsigned(bullet_y) - 1);
+                        curr_bullet_y <= std_logic_vector(unsigned(curr_bullet_y) - 1);
                     end if;
-                    -- Set internal_bullet_status to 1 since it is still valid
-                    internal_bullet_status <= '1';
+                    -- Keep x value of bullet the same
+                    curr_bullet_x <= curr_bullet_x;
                     -- Set hit to 0
-                    hit <= '0';
+                    internal_hit <= '0';
+                    -- Bullet is still valid
+                    internal_bullet_valid <= '1';
                 -- If bullet is not in a valid location
                 else
-                    -- Set internal_bullet_status to 0 since it is no longer valid
-                    internal_bullet_status <= '0';
-                    -- Set hit to 1
-                    hit <= '0';
-                end if;
+                    -- Set hit to 0
+                    internal_hit <= '0';
+                    -- Set x value of bullet to x value of tank
+                    curr_bullet_x <= curr_tank_x;
+                    -- Set y value of bullet to y value of tank
+                    curr_bullet_y <= curr_tank_y;
+                    -- Bullet is no longer valid
+                    internal_bullet_valid <= '0';
+                end if; 
             end if;
         end if;
 
     end process;
+
+    bullet_x <= curr_bullet_x;
+    bullet_y <= curr_bullet_y;
+    hit <= internal_hit;
 
 end architecture behavioral;
--- bullets/bullets_test.vhd
+++ bullets/bullets_test.vhd
@@ -3,82 +3,95 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.std_logic_textio.all;
 use std.textio.all;
+use WORK.tank_const.all;
 
-entity tanks_tb is
+entity bullets_tb is
 	
-end entity tanks_tb;
+end entity bullets_tb;
 
-architecture behavioural of tanks_tb is
+architecture behavioural of bullets_tb is
 
-	--Tank component
-    component tank is
+	-- Bullet component
+    component bullet is
+
         port (
             -- Inputs
-            clk : in std_logic;
+            bullet_move : in std_logic;
             rst : in std_logic;
-            tank_in_y : in std_logic_vector (9 downto 0);
-            tank_move : in std_logic;
-            tank_out_y : in std_logic_vector (9 downto 0);
-            tank_out_x : in std_logic_vector (9 downto 0);
+            rst_hit : in std_logic;
+            shoot : in std_logic;
+            curr_tank_x : in std_logic_vector (9 downto 0);
+            curr_tank_y : in std_logic_vector (9 downto 0);
+            other_tank_x : in std_logic_vector (9 downto 0);
+            other_tank_y : in std_logic_vector (9 downto 0);
+            -- Outputs
+            bullet_x : out std_logic_vector (9 downto 0);
+            bullet_y: out std_logic_vector (9 downto 0);
+            hit : out std_logic
         );
-        generic (
-            screen_width : natural := 640;
-            screen_height : natural := 480;
-            tank_width: natural := 20;
-            tank_height: natural := 10;
-        );
-    end component tank;
+    
+    end component bullet;
 
 	--Declaring signals
-    signal clk_TB : in std_logic := '0';
-    signal rst_TB : in std_logic := '0';
-    signal tank_in_y_TB : in std_logic_vector (9 downto 0);
-    signal tank_move_TB : in std_logic;
-    signal tank_out_y_TB : in std_logic_vector (9 downto 0);
-    signal tank_out_x_TB : in std_logic_vector (9 downto 0);
+    signal bullet_move_TB : std_logic := '0';
+    signal rst_TB : std_logic := '0';
+    signal shoot_TB : std_logic := '0';
+    signal curr_tank_x_TB : std_logic_vector (9 downto 0);
+    signal curr_tank_y_TB : std_logic_vector (9 downto 0);
+    signal other_tank_x_TB : std_logic_vector (9 downto 0);
+    signal other_tank_y_TB : std_logic_vector (9 downto 0);
+    signal bullet_x_TB : std_logic_vector (9 downto 0);
+    signal bullet_y_TB : std_logic_vector (9 downto 0);
+    signal hit_TB : std_logic;
+    signal rst_hit_TB : std_logic;
 
-	--Signals for writing to result to output file
-	signal internal_quotient_TB : std_logic_vector (DIVIDEND_WIDTH - 1 downto 0);
-	signal internal_overflow_TB : std_logic;
-	signal internal_remainder_TB: std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
-	
 	--Signal to determine when testbench is done
 	signal TB_done : std_logic;
+    signal store_hit_TB : std_logic;
 
 	--File Variables
 	file infile : text open read_mode is "bullets_test.in";
-	file outfile : text open write_mode is ".out";
+	file outfile : text open write_mode is "bullets_test.out";
 
 begin
 
-    dut: tank
-        port (
+    dut: bullet
+        port map (
             -- Inputs
-            clk => clk_TB,
+            bullet_move => bullet_move_TB,
             rst => rst_TB,
-            tank_in_y => tank_in_y_TB,
-            tank_move => tank_move_TB,
-            tank_out_y => tank_out_y_TB,
-            tank_out_x => tank_out_x_TB
-        );
-        generic (
-            screen_width => 640,
-            screen_height => 480,
-            tank_width => 20,
-            tank_height => 10
+            rst_hit => rst_hit_TB,
+            shoot => shoot_TB,
+            curr_tank_x => curr_tank_x_TB,
+            curr_tank_y => curr_tank_y_TB,
+            other_tank_x => other_tank_x_TB,
+            other_tank_y => other_tank_y_TB,
+            -- Outputs
+            bullet_x => bullet_x_TB,
+            bullet_y => bullet_y_TB,
+            hit => hit_TB
         );
 					
 	process begin
-		clk_TB <= not clk_TB;
-		wait for 1 ps;	
+		bullet_move_TB <= not bullet_move_TB;
+		wait for 1 ps;
 	end process;
+
+    process (hit_TB) begin
+        if (rising_edge(hit_TB)) then
+            store_hit_TB <= '1';
+        end if;
+    end process;
 
 				
 	process is
 	-- Variables for reading and writing strings
 	variable my_line : line;
-	variable term_one : integer;
-	variable term_two : integer;
+	variable in_curr_tank_x : integer;
+	variable in_curr_tank_y : integer;
+    variable in_other_tank_x : integer;
+	variable in_other_tank_y : integer;
+    variable shoot_cycles : integer;
 	
 	--Begin testbench
 	begin
@@ -91,37 +104,73 @@ begin
 		while not endfile(infile) loop
 			--Reading the file and preparing the beginning of the ouput
 			readline(infile, my_line);
-			-- First term: starting y position
-			read(my_line, term_one);
+			-- First term: curr_tank_x
+			read(my_line, in_curr_tank_x);
 			readline(infile, my_line);
-			-- Second term: number of move calls
-			read(my_line, term_two);	
-			
-			rst_TB <= '0';
-			--Feeding input for tank
-			tank_in_y_TB <= std_logic_vector(to_signed(term_one, 10));
+			-- Second term: curr_tank_y
+			read(my_line, in_curr_tank_y);
+            readline(infile, my_line);
+			-- Third term: other_tank_x
+			read(my_line, in_other_tank_x);
+			readline(infile, my_line);
+			-- Fourth term: other_tank_y
+			read(my_line, in_other_tank_y);	
+            readline(infile, my_line);
+            -- Fifth term: move_cycles
+			read(my_line, shoot_cycles);	
 
+            --Feeding input for bullet
+            curr_tank_x_TB <= std_logic_vector(to_unsigned(in_curr_tank_x, curr_tank_x_TB'length));
+            curr_tank_y_TB <= std_logic_vector(to_unsigned(in_curr_tank_y, curr_tank_y_TB'length));
+            other_tank_x_TB <= std_logic_vector(to_unsigned(in_other_tank_x, other_tank_x_TB'length));
+            other_tank_y_TB <= std_logic_vector(to_unsigned(in_other_tank_y, other_tank_y_TB'length));
+			
+            rst_hit_TB <= '0';
+			rst_TB <= '0';
 			wait for 2 ps;
 			rst_TB <= '1';
 			wait for 6 ps;
 			rst_TB <= '0';
-			wait for 2 ps;
+			wait for 42 ps;
+
+			std.textio.write(my_line, string'("Curr Tank X: "));
+			std.textio.write(my_line, to_integer(unsigned(curr_tank_x_TB)));
+            std.textio.write(my_line, string'(", Curr Tank Y: "));
+			std.textio.write(my_line, to_integer(unsigned(curr_tank_y_TB)));
+            std.textio.write(my_line, string'(", Other Tank X: "));
+			std.textio.write(my_line, to_integer(unsigned(other_tank_x_TB)));
+            std.textio.write(my_line, string'(", Other Tank Y: "));
+			std.textio.write(my_line, to_integer(unsigned(other_tank_y_TB)));
 			
 			-- Move tank
-			for i in 0 to term_two loop
-				tank_move_TB <= not tank_move_TB;
-				wait for 5 ps;
+			for i in 0 to shoot_cycles loop
+				shoot_TB <= not shoot_TB;
+				wait for 0.1 ps;
+				shoot_TB <= not shoot_TB;
+				wait for 497.9 ps;
+                if (hit_TB = '1') then
+                    rst_hit_TB <= '1';
+                    wait for 1 ps;
+                    rst_hit_TB <= '0';
+                    wait for 1 ps;
+                else
+                    rst_hit_TB <= '0';
+                    wait for 2 ps;
+                end if;
 			end loop;
 
 			--With the calculator result, prepare output for outfile
-			std.textio.write(my_line, string'("Input Tank Y: "));
-			std.textio.write(my_line, to_integer(unsigned(tank_in_y_TB)));
-			std.textio.write(my_line, string'(", Cycles: "));
-			std.textio.write(my_line, termtwo);
-			std.textio.write(my_line, string'(", Final Tank Y: "));
-			std.textio.write(my_line, to_integer(unsigned(tank_out_y)));
-			std.textio.write(my_line, string'(", Final Tank X: "));
-			std.textio.write(my_line, to_integer(unsigned(tank_out_x)));
+			
+			std.textio.write(my_line, string'(", Hit: "));
+            if (store_hit_TB = '1') then
+                std.textio.write(my_line, string'("Yes "));
+            else
+                std.textio.write(my_line, string'("No "));                
+            end if;
+			std.textio.write(my_line, string'(", Final Bullet X: "));
+			std.textio.write(my_line, to_integer(unsigned(bullet_x_TB)));
+			std.textio.write(my_line, string'(", Y: "));
+			std.textio.write(my_line, to_integer(unsigned(bullet_y_TB)));
 			writeline(outfile, my_line);
 			
 			wait for 5 ps;
