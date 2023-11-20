e6c6ee276f07d2823df8fdf35064dae81d0f57ab
onecholas <nickhsu2025@u.northwestern.edu>
2023-11-20 14:34:49 -0600

    Score tested and working

--- score/score.vhd
+++ score/score.vhd
@@ -9,26 +9,30 @@ entity score is
     port (
         rst : in std_logic;
         other_tank_hit : in std_logic;
-        curr_tank_score : out std_logic_vector (1 downto 0);
+        curr_tank_score : out std_logic_vector (1 downto 0)
     );
 
 end entity score;
 
 architecture behavioral of score is
 
+    signal internal_curr_tank_score : std_logic_vector (1 downto 0);
+
 begin
 
-    process (hit, rst) begin
+    process (other_tank_hit, rst) begin
         -- If reset = '1'
         if (rst = '1') then
             -- Reset score to 0
-            curr_tank_score <= (others => '0');
+            internal_curr_tank_score <= (others => '0');
         -- If hit becomes 1
-        elsif (rising_edge(hit)) then
+        elsif (rising_edge(other_tank_hit)) then
             -- Increase score by 1
-            curr_tank_score <= std_logic_vector(to_unsigned(curr_tank_score) + 1);
+            internal_curr_tank_score <= std_logic_vector(unsigned(internal_curr_tank_score) + to_unsigned(1, internal_curr_tank_score'length));
         end if;
 
     end process;
+
+    curr_tank_score <= internal_curr_tank_score;
 
 end architecture behavioral;
